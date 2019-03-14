96f71683aa18455596cab664a58c71b911f95957
Bruna Vieira Teixeira <bruna.v.teixeira@accenture.com>
2019-03-14 17:19:51 -0300

    add post

---
+++ src/actions/posts.js
@@ -0,0 +1,66 @@
+import uuid from "uuid";
+import { APIaddposts } from "../api";
+import { APIfetchPost } from "../api";
+import { APIdelPosts } from "../api";
+export const RECEIVE_POSTS = "RECEIVE_POSTS";
+export const LOAD_POST = "LOAD_POST";
+export const ADD_POST = "ADD_POST";
+export const EDIT_POST = "EDIT_POST";
+export const DELETE_POST = "DELETE_POST";
+
+
+export function receivePosts(posts) {
+  return {
+    type: RECEIVE_POSTS,
+    posts
+  }
+}
+
+export function loadPost(post) {
+  return {
+    type: LOAD_POST,
+    post
+  }
+}
+
+export const fetchPost = (id) => {
+  return dispatch => {
+    return APIfetchPost(id).then(pid => {
+      dispatch(loadPost(pid))
+    })
+  }
+}
+
+export const deletePost = (id) => {
+  return {
+    type: DELETE_POST,
+    id,
+  }
+}
+
+export const delpost = (idpost) => {
+  return dispatch => {
+    return APIdelPosts(idpost).then(idp => {
+      dispatch(deletePost(idp));
+    })
+  }
+}
+
+
+export const createpost = newpost => {
+  const post = {
+    ...newpost,
+    timestamp: Date.now(),
+    id: uuid(),
+  }
+  return dispatch => {
+    return APIaddposts(post).then(newp => {
+      dispatch(addPost( newp ));
+    });
+  };
+};
+
+export const addPost = (newpost) => ({
+  type: ADD_POST,
+  newpost
+});
---
+++ src/api.js
@@ -0,0 +1,72 @@
+import axios from 'axios';
+
+// ajuda para entender request do axios mentor Eric Gruby
+
+axios.defaults.headers.common['Authorization'] = 'qualquer_coisa';
+
+const API_URL = 'http://localhost:3001';
+
+//Ajuda com criação dos requests Cláudio Rocha
+
+export const fetchComments = (postId) =>
+  axios({
+    url: API_URL + `/posts/${postId}/comments`,
+    method: "GET"
+  }).then(response => {
+    //console.log(response.data);
+    return response.data;
+  });
+
+
+export const fetchCategories = () =>
+  axios({
+    url: API_URL + '/categories',
+    method: 'GET',
+  }).then(response => {
+    return response.data;
+  });
+
+
+const getPosts = () =>
+  axios({
+    url: API_URL + '/posts',
+    method: 'GET',
+  }).then(response => {
+    return response.data;
+  });
+
+export const APIfetchPost = (id) => {
+  axios({
+    url: API_URL + `/posts/${id}`,
+    method: 'GET',
+  }).then(response => {
+    return response.data;
+  })
+}
+
+
+export const APIaddposts = (newpost) => {
+  console.log(newpost)
+  return (
+    axios({
+      url: API_URL + '/posts',
+      method: 'POST',
+      data: {
+        ...newpost
+      }
+    })
+  )
+}
+
+export const APIdelPosts = id =>
+  axios({
+    url: API_URL + `/posts/${id}`,
+    method: 'DELETE',
+    data: {
+      id
+    }
+  }).then(response => {
+    return response.data;
+  });
+
+export default getPosts;
